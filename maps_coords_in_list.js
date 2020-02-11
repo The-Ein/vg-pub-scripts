e940386ec9c010c3912f6c55aa6f22c74d3b3fc2
The-Ein <34685124+The-Ein@users.noreply.github.com>
2020-02-12 01:27:59 +0300

    setTimeout replaced with Promise

--- maps_coords_in_list.js
+++ maps_coords_in_list.js
@@ -1,24 +1,27 @@
-(function(){
-	function aget(url, cb){
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', url, true);
-		xhr.onreadystatechange = function() {
-		  if (xhr.readyState != 4) return;
-		  if (xhr.status != 200) {
-			throw new Error(xhr.status + ': ' + xhr.statusText);
-		  } else {
-			(typeof cb === 'function' ? cb : function(){})(xhr.responseText);
-		  }
-		}
-		xhr.send();
-	}
+(async function(){
+    let links = document.querySelectorAll('a');
+    for(let i = 0; i < links.length; i++){
+        let link = links[i];
+        if(!link.innerText.match(/Карта сокровищ/)) continue;
 
-	var links_raw = document.querySelectorAll('a');
-	for(let i = 0; i < links_raw.length; i++){
-		if(!links_raw[i].innerText.match(/Карта сокровищ/)) continue;
-		setTimeout(()=>{aget(links_raw[i].href, function(resp){
-			var coords = resp.match(/тут: (\d+\/\d+)/)[1];
-			links_raw[i].innerHTML = links_raw[i].innerHTML.replace(/(сокровищ)/, '$1 '+coords);
-		})},i*150);
+        let resp = await aget(link.href);
+        let coords = resp.match(/тут: (\d+\/\d+)/)[1];
+		link.innerHTML = link.innerHTML.replace(/(сокровищ)/, '$1 '+coords);
+    }
+
+    function aget(url){
+		return new Promise((resolve, reject) => {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.onreadystatechange = function() {
+              if (xhr.readyState != 4) return;
+              if (xhr.status != 200) {
+                reject(xhr.status + ': ' + xhr.statusText);
+              } else {
+                resolve(xhr.responseText);
+              }
+            }
+            xhr.send();
+        });
 	}
 })();
